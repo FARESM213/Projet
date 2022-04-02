a812278854e4e79cc8624cd4e00afb19390d6f86
Malena <malena.paz@edu.ece.fr>
2022-04-02 23:38:18 +0200

    WELCOME2 + Creation compte

--- src/main/java/Controller/CreationCompte.java
+++ src/main/java/Controller/CreationCompte.java
@@ -174,8 +174,10 @@ public class CreationCompte {
     public static boolean isValidPassword(String password)
     {
 
-        String regex = "(?=.*[0-9])"
-                + "(?=.*[a-z])(?=.*[A-Z])";
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
 
         // Compile the ReGex
         Pattern p = Pattern.compile(regex);
